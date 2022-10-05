e34f2c99963e62241c3e780f3374ffe05742e540
mengzhey <106492397+mengzhey@users.noreply.github.com>
2022-10-05 17:49:57 -0400

    1005

--- solution-hw5/js/cartdetail.js
+++ solution-hw5/js/cartdetail.js
@@ -1,17 +1,14 @@
-
-
-
 let rollGlazingPrice = {
     "Original": {
         "price": 0,
     },
-    "Sugar-milk": {
+    "Sugar milk": {
         "price": 0,
     },
-    "Vanilla-milk": {
+    "Vanilla milk": {
         "price": 0.5,
     },
-    "Double-chocolate": {
+    "Double chocolate": {
         "price": 1.5,
     },
   
@@ -49,54 +46,17 @@ class Roll {
     }
 }
 
-const rollSet = new Set();
+const rollSet = new Array();
     
 
 function addNewRoll(imageURL, rollType, rollGlazing, packSize, rollPrice, rollCalPrice){
    
    const cartRoll = new Roll(imageURL,rollType, rollGlazing, packSize, rollPrice, rollCalPrice);
-   rollSet.add(cartRoll);
+   rollSet.push(cartRoll);
    return cartRoll;    
 }
 
-const cartOriginal = addNewRoll(
-    './assets/original-cinnamon-roll.jpg',
-    'Original',
-    'Sugar-milk',
-    '1',
-    '2.49',
-    
 
-);
-
-const cartWalnut = addNewRoll(
-    './assets/walnut-cinnamon-roll.jpg',
-    'Walnut',
-    'Vanilla-milk',
-    '12',
-    '3.49',
-);
-
-const cartRaisin = addNewRoll(
-    './assets/raisin-cinnamon-roll.jpg',
-    'Raisin',
-    'Sugar-milk',
-    '3',
-    '2.99',
-);
-
-const cartApple = addNewRoll(
-    './assets/apple-cinnamon-roll.jpg',
-    'Apple',
-    'Original',
-    '3',
-    '3.49',
-);
-
-for (const cartRoll of rollSet) {
-    console.log(cartRoll);
-    createElement(cartRoll);
-  }
 
 function createElement(cartRoll){
     const template = document.querySelector('#roll-template')
@@ -113,18 +73,71 @@ function updateElement(cartRoll) {
     const packElement = cartRoll.element.querySelector('.cartPack');
     const cartTitleElement = cartRoll.element.querySelector('.cartTitle');
     const cartImageElement = cartRoll.element.querySelector('#cart-image');
+    const glazingElement = cartRoll.element.querySelector('.cartGlazing');
+    // const totalPriceElement = document.querySelector('.price');
 
     const btnDelete = cartRoll.element.querySelector('#removeIcon')
     btnDelete.addEventListener('click', () => {
-        deleteNote(cartRoll);
+        deleteRoll(cartRoll);
     });
+    
+  
+    // totalPriceElement.innerText = totalFinalPrice;
     priceElement.innerText = cartRoll.rollCalPrice;
     packElement.innerText = 'Pack Size:' + cartRoll.size;
     cartTitleElement.innerText = cartRoll.type + ' Cinnammon Roll';
     cartImageElement.src = cartRoll.rollimageURL;
+    glazingElement.innerText = cartRoll.glazing;
+    
 }
 
-function deleteNote(cartRoll) {
+function deleteRoll(cartRoll) {
     cartRoll.element.remove();
-    rollSet.delete(cartRoll);
+    rollSet.pop(cartRoll);
 }
+
+// function sumRoll(cartRoll) {
+//   const totalFinalPrice = cartRoll.rollCalPrice;
+  
+//   const totalPriceElement = document.querySelector('.price');
+//   totalPriceElement.innerText = totalFinalPrice;
+  
+// }
+
+const cartOriginal = addNewRoll(
+    './assets/original-cinnamon-roll.jpg',
+    'Original',
+    'Sugar milk',
+    '1',
+    '2.49',
+);
+
+const cartWalnut = addNewRoll(
+    './assets/walnut-cinnamon-roll.jpg',
+    'Walnut',
+    'Vanilla milk',
+    '12',
+    '3.49',
+);
+
+const cartRaisin = addNewRoll(
+    './assets/raisin-cinnamon-roll.jpg',
+    'Raisin',
+    'Sugar milk',
+    '3',
+    '2.99',
+);
+
+const cartApple = addNewRoll(
+    './assets/apple-cinnamon-roll.jpg',
+    'Apple',
+    'Original',
+    '3',
+    '3.49',
+);
+
+for (const cartRoll of rollSet) {
+    console.log(cartRoll);
+    createElement(cartRoll);
+  }    
+
